72b6ce4356f31315ba53d3ad71aa34747d2179b7
Freeder <Freeder@rupuishplus.com>
2018-03-09 12:00:08 +0800

    merge 代码

--- app/src/main/java/com/yinshan/happycash/utils/ToastUtils.java
+++ app/src/main/java/com/yinshan/happycash/utils/ToastUtils.java
@@ -3,49 +3,40 @@ package com.yinshan.happycash.utils;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.yinshan.happycash.application.AppApplication;
+
 /**
  * Created by admin on 2018/1/30.
  */
 
 public class ToastUtils {
 
-    public static Context mContext;
+    private static Context mContext;
     private static Toast toast;
-
-
-    private ToastUtils() {
-        /* cannot be instantiated */
-        throw new UnsupportedOperationException("cannot be instantiated");
-    }
-
-
-    public static void register(Context context) {
-        mContext = context;
-    }
 
     /**
      * 短时间显示Toast
      */
     public static void showShort(CharSequence message) {
         if (mContext==null){
-            throw new RuntimeException("unRegister Context in Application");
+            mContext= AppApplication.appContext;
         }
         if (toast != null) {
             toast.cancel();
         }
-        toast = Toast.makeText(mContext, message, Toast.LENGTH_LONG);
+        toast = Toast.makeText(mContext, message, Toast.LENGTH_SHORT);
         toast.setText(message);
         toast.show();
     }
 
     public static void showShort(int resId) {
         if (mContext==null){
-            throw new RuntimeException("unRegister Context in Application");
+            mContext= AppApplication.appContext;
         }
         if (toast != null) {
             toast.cancel();
         }
-        toast = Toast.makeText(mContext, mContext.getString(resId), Toast.LENGTH_LONG);
+        toast = Toast.makeText(mContext, mContext.getString(resId), Toast.LENGTH_SHORT);
         toast.setText(mContext.getString(resId));
         toast.show();
     }
