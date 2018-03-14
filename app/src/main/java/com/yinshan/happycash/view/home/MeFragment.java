c6aff6f7c12f56be26fbd458f6b8a9b5cf939b6b
huxin <huxin@rupiahplus.com>
2018-03-14 17:28:55 +0800

    ui

---
+++ app/src/main/java/com/yinshan/happycash/view/home/MeFragment.java
@@ -0,0 +1,20 @@
+package com.yinshan.happycash.view.home;
+
+import com.yinshan.happycash.framework.BaseFragment;
+
+/**
+ * Created by huxin on 2018/3/13.
+ */
+
+public class MeFragment extends BaseFragment{
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int bindLayout() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/yinshan/happycash/view/me/UploadPhotoActivity.java
@@ -0,0 +1,24 @@
+package com.yinshan.happycash.view.me;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.yinshan.happycash.R;
+import com.yinshan.happycash.framework.BaseActivity;
+
+/**
+ * Created by huxin on 2018/3/14.
+ */
+
+public class UploadPhotoActivity extends BaseActivity{
+
+    @Override
+    protected void initView(View view, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int bindLayout() {
+        return R.layout.activity_upload_photo;
+    }
+}
