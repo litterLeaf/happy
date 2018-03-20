6900fed3840944c37f1d0e75350e7430c50d36ef
Freeder <Freeder@rupuishplus.com>
2018-03-20 16:01:12 +0800

    主体按钮模块

---
+++ app/src/main/java/com/yinshan/happycash/view/unloan/UnLoanFragment.java
@@ -0,0 +1,46 @@
+package com.yinshan.happycash.view.unloan;
+
+import android.os.Bundle;
+
+import com.yinshan.happycash.R;
+import com.yinshan.happycash.framework.BaseFragment;
+
+
+/**
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ *        ┃　　　┃   神兽保佑
+ *        ┃　　　┃   代码无BUG！
+ *        ┃　　　┗━━━┓
+ *        ┃　　　　　　　┣┓
+ *        ┃　　　　　　　┏┛
+ *        ┗┓┓┏━┳┓┏┛
+ *           ┃┫┫　┃┫┫
+ *           ┗┻┛　┗┻┛
+ *
+ *  @author  admin
+ *  on 2018/1/31
+ *
+ */
+
+public class UnLoanFragment extends BaseFragment {
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int bindLayout() {
+        return R.layout.fragment_main_unloan;
+    }
+
+
+}
