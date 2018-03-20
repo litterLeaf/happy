88d9cceab02cda5c196e4802a5548fb60f488b71
huxin <huxin@rupiahplus.com>
2018-03-20 18:40:53 +0800

    UI

---
+++ app/src/main/java/com/yinshan/happycash/view/me/presenter/LoanListPresenter.java
@@ -0,0 +1,12 @@
+package com.yinshan.happycash.view.me.presenter;
+
+/**
+ * Created by huxin on 2018/3/20.
+ */
+
+public class LoanListPresenter {
+
+
+}
+
+
---
+++ app/src/main/java/com/yinshan/happycash/view/me/presenter/UploadPresenter.java
@@ -0,0 +1,8 @@
+package com.yinshan.happycash.view.me.presenter;
+
+/**
+ * Created by huxin on 2018/3/20.
+ */
+
+public class UploadPresenter {
+}
