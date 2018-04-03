406b71ff8b2b1f177b626d727789674c38eb8f63
huxin <huxin@rupiahplus.com>
2018-04-03 14:17:28 +0800

    update

---
+++ app/src/main/java/com/yinshan/happycash/view/loan/view/IApplyView.java
@@ -0,0 +1,10 @@
+package com.yinshan.happycash.view.loan.view;
+
+/**
+ * Created by huxin on 2018/4/3.
+ */
+
+public interface IApplyView {
+
+
+}
---
+++ app/src/main/java/com/yinshan/happycash/view/loan/view/impl/ApplyFragment.java
@@ -0,0 +1,20 @@
+package com.yinshan.happycash.view.loan.view.impl;
+
+import com.yinshan.happycash.framework.BaseFragment;
+
+/**
+ * Created by huxin on 2018/4/3.
+ */
+
+public class ApplyFragment extends BaseFragment{
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int bindLayout() {
+        return 0;
+    }
+}
