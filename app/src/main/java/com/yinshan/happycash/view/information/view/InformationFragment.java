cd84c5fe4634b3cf16fc51f435c5d753115a366a
huxin <huxin@rupiahplus.com>
2018-04-08 17:02:01 +0800

    微调UI

--- app/src/main/java/com/yinshan/happycash/view/information/view/InformationFragment.java
+++ app/src/main/java/com/yinshan/happycash/view/information/view/InformationFragment.java
@@ -10,8 +10,10 @@ import android.widget.TextView;
 
 import com.yinshan.happycash.R;
 import com.yinshan.happycash.framework.BaseFragment;
+import com.yinshan.happycash.view.information.view.impl.ContactActivity;
 import com.yinshan.happycash.view.information.view.impl.JobInformation;
 import com.yinshan.happycash.view.information.view.impl.PersonalInformation;
+import com.yinshan.happycash.view.information.view.impl.UploadPhotoActivity;
 import com.yinshan.happycash.view.main.MainActivity;
 import com.yinshan.happycash.widget.userdefined.ProfilProgressView;
 
@@ -109,12 +111,16 @@ public class InformationFragment extends BaseFragment {
                 ((MainActivity)getActivity()).mStartActivity(getActivity(), JobInformation.class);
                 break;
             case R.id.view_not_finish_contact:
+                ((MainActivity)getActivity()).mStartActivity(getActivity(), ContactActivity.class);
                 break;
             case R.id.view_finish_contact:
+                ((MainActivity)getActivity()).mStartActivity(getActivity(), ContactActivity.class);
                 break;
             case R.id.view_not_finish_upload_photo:
+                ((MainActivity)getActivity()).mStartActivity(getActivity(), UploadPhotoActivity.class);
                 break;
             case R.id.view_finish_upload_photo:
+                ((MainActivity)getActivity()).mStartActivity(getActivity(), UploadPhotoActivity.class);
                 break;
         }
     }
