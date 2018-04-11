263904107b1dfa7ce2cacb907fe0a8c7bd85abe6
huxin <huxin@rupiahplus.com>
2018-04-11 16:06:33 +0800

    联系人

---
+++ app/src/main/java/com/yinshan/happycash/view/information/model/ContactBean.java
@@ -0,0 +1,36 @@
+package com.yinshan.happycash.view.information.model;
+
+/**
+ * Created by huxin on 2018/4/10.
+ */
+
+public class ContactBean {
+
+    private String mobile;
+    private String name;
+    private String relation;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRelation() {
+        return relation;
+    }
+
+    public void setRelation(String relation) {
+        this.relation = relation;
+    }
+}
--- app/src/main/java/com/yinshan/happycash/view/information/presenter/ContactPresenter.java
+++ app/src/main/java/com/yinshan/happycash/view/information/presenter/ContactPresenter.java
@@ -1,10 +1,51 @@
 package com.yinshan.happycash.view.information.presenter;
 
+import android.content.Context;
+
+import com.yinshan.happycash.framework.TokenManager;
+import com.yinshan.happycash.network.api.RecordApi;
+import com.yinshan.happycash.network.common.RxHttpUtils;
+import com.yinshan.happycash.network.common.base.ApiException;
+import com.yinshan.happycash.network.common.base.BaseObserver;
+import com.yinshan.happycash.network.common.base.RxTransformer;
+import com.yinshan.happycash.view.information.model.ContactBean;
+import com.yinshan.happycash.view.information.view.IContactView;
+
+import java.lang.ref.SoftReference;
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
 /**
  * Created by huxin on 2018/4/2.
  */
 
 public class ContactPresenter {
 
+    Context mContext;
+    IContactView mView;
 
+    public ContactPresenter(Context context,IContactView view){
+        mContext = context;
+        mView = view;
+    }
+
+    public void getContactInfo(){
+        RxHttpUtils.getInstance().createApi(RecordApi.class)
+                .getContactInfo(TokenManager.getInstance().getToken())
+                .compose(RxTransformer.io_main())
+                .subscribe(new BaseObserver<List<ContactBean>>(new SoftReference(mContext)){
+                    @Override
+                    public void onNext(List<ContactBean> value) {
+                        super.onNext(value);
+                        mView.showContactInfo(value);
+                    }
+
+                    @Override
+                    protected void onError(ApiException ex) {
+                        super.onError(ex);
+                    }
+                });
+    }
 }
