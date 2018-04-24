11270f734fe3b46739a4218c9fe16747e1ec373c
Freeder <Freeder@rupuishplus.com>
2018-04-24 11:10:36 +0800

    tabBar的整合

--- app/src/main/java/com/yinshan/happycash/view/main/SplashActivity.java
+++ app/src/main/java/com/yinshan/happycash/view/main/SplashActivity.java
@@ -26,107 +26,37 @@ import com.yinshan.happycash.view.main.presenter.SplashPresenter;
  */
 
 public class SplashActivity extends AppCompatActivity implements SplashContract.View{
-    private Long              mStartTime;
-    private Handler           mHandler;
-    private boolean canFinish= true;
-    private boolean isVisible = true;
     private SplashContract.Presenter splashPresenter;
     private LastLoanAppBean mLatestLoanAppBean;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mStartTime = SystemClock.currentThreadTimeMillis();
-        mHandler = new Handler();
-        jumpToMainActivity(mStartTime);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
-
         splashPresenter = new SplashPresenter(this);
         splashPresenter.attachView(this);
-        if (!TokenManager.getInstance().hasLogin()) {
-            canFinish = true;
-            jumpToMainActivity(SystemClock.currentThreadTimeMillis());
-        } else {
-            splashPresenter.getLastLoanAppBean(TokenManager.getInstance().getToken());
-        }
-    }
-
-    private void jumpToMainActivity(Long currentTime) {
-        Long delayTime = (currentTime - mStartTime >= 3000L) ? 0 : (mStartTime + 3000 - currentTime);
-        mHandler.postDelayed(() -> {
-//            if(TextUtils.isEmpty(password)){
-//                if(TextUtils.isEmpty(TokenManager.getInstance().getToken())){
-                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
-                    if (canFinish) {
-                        mHandler.removeCallbacksAndMessages(null);
-                        finish();
-                    } else {
-                        //                    setVisible(false);
-                        isVisible = false;
-                    }
-//                }else {
-//                    Intent intent = new Intent(SplashActivity.this, SetPatternActivity.class);
-//                    intent.putExtra("splash",true);
-//                    startActivity(intent);
-//                    if (canFinish) {
-//                        mHandler.removeCallbacksAndMessages(null);
-//                        finish();
-//                    } else {
-//                        //                    setVisible(false);
-//                        isVisible = false;
-//                    }
-//                }
-//            }else {
-//                Intent intent = new Intent(SplashActivity.this, VerifyPatternActivity.class);
-//                startActivity(intent);
-//                if (canFinish) {
-//                    mHandler.removeCallbacksAndMessages(null);
-//                    finish();
-//                } else {
-//                    //                    setVisible(false);
-//                    isVisible = false;
-//                }
-//            }
-        },delayTime);
     }
 
     @Override
     public void getStatusSuccess(LastLoanAppBean latestLoanAppBean) {
         mLatestLoanAppBean = latestLoanAppBean;
         DateManager dateManager = DateManager.getInstance();
-        if(latestLoanAppBean ==null){
+        if(mLatestLoanAppBean ==null){
             if(!TokenManager.getInstance().hasLogin()){
                 dateManager.putToCache(SPKeyUtils.APP_STATUES, AppLoanStatus.UNLOAN);
             }
         }else {
-            dateManager.putToCache(SPKeyUtils.LOANAPPBEAN,latestLoanAppBean);
-//            dateManager.putToCache(SPKeyUtils.STATUES,latestLoanAppBean.getStatus());
-            dateManager.putToCache(SPKeyUtils.SERVER_STATUES, latestLoanAppBean.getStatus());
-            dateManager.putToCache(SPKeyUtils.APP_STATUES, StatusManagementUtils.loanStatusClassify(latestLoanAppBean));
-            StatusManagementUtils.loanStatusClassify(latestLoanAppBean);
-        }
-        canFinish = true;
-        if (SplashActivity.this.isVisible) {
-            jumpToMainActivity(SystemClock.currentThreadTimeMillis());
-        } else {
-            mHandler.removeCallbacksAndMessages(null);
-            finish();
+            dateManager.putToCache(SPKeyUtils.LOANAPPBEAN,mLatestLoanAppBean);
+            dateManager.putToCache(SPKeyUtils.SERVER_STATUES, mLatestLoanAppBean.getStatus());
+            dateManager.putToCache(SPKeyUtils.APP_STATUES, StatusManagementUtils.loanStatusClassify(mLatestLoanAppBean));
+            StatusManagementUtils.loanStatusClassify(mLatestLoanAppBean);
         }
     }
 
     @Override
     public void getStatusError(String message) {
         DateManager.getInstance().putToCache(SPKeyUtils.APP_STATUES, AppLoanStatus.UNLOAN);
-        canFinish = true;
-        if (SplashActivity.this.isVisible) {
-            jumpToMainActivity(SystemClock.currentThreadTimeMillis());
-        } else {
-            mHandler.removeCallbacksAndMessages(null);
-            finish();
-        }
     }
 }
