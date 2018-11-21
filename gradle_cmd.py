1bbd8f8f70db37336b1c9e368fbaa0f70aac131e
Lester <liuwanlin@rupiahplus.com>
2018-11-21 14:41:37 +0800

    完善打包脚本

--- gradle_cmd.py
+++ gradle_cmd.py
@@ -1,10 +1,25 @@
 # -*- coding:utf-8 -*-
-import os
+import os, shutil
+
+# 打包默认生成的apk路径
+apk = './app/build/outputs/apk/release/app-release.apk'
+# 打包完成复制到的目标路径 按需修改
+copyApk = 'C:/apks/app-release.apk'
 
 
 def execute_cmd():
     # tmp = os.popen('gradlew assembleRelease')
     # print(tmp)
-    os.system('gradlew assembleRelease')
+    # os.system('gradlew assembleRelease')
+    output = os.popen('gradlew assembleRelease')
+    ret = output.read()
+    print(ret)
+    if 'BUILD SUCCESSFUL' in ret:
+        print('打包成功')
+        shutil.copyfile(apk, copyApk)
+    else:
+        print('打包失败')
 
-execute_cmd()
+
+if __name__ == '__main__':
+    execute_cmd()
